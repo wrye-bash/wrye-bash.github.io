09f78fc47aa2f105f5a40b29475eb0e0bdcc91c0
Brandon Dunneman <bwfang1235@gmail.com>
2019-02-26 00:48:38 -0800

    Let's take this step by step, shall we?
    * Linted the slideshow javascript and made it slightly more efficient. Slightly.
    * That little baby's gonna get ripped out in the future, but for now, let's make it look pretty!

--- docs/assets/bashfunc.js
+++ docs/assets/bashfunc.js
@@ -1,15 +1,18 @@
+/*jslint browser:true*/
+"use strict";
+
 function nextImage(imgs) {
-    for (var i = 0; i < imgs.length; i++) {
-        if (imgs[0].style.opacity !== '0' && imgs[0].style.opacity !== '1') {
-            imgs[0].style.opacity = '1';
+    for (let i = 0; i < imgs.length; i++) {
+        if (imgs[0].style.opacity == "") {
+            imgs[0].style.opacity = "1";
             break;
         }
-        else if (imgs[i].style.opacity == '1' || imgs[i].style.opacity == '') {
-            imgs[i].style.opacity = '0';
+        else if (imgs[i].style.opacity == "1" || imgs[i].style.opacity == "") {
+            imgs[i].style.opacity = "0";
             if (i + 1 < imgs.length) {
-                imgs[i + 1].style.opacity = '1';
+                imgs[i + 1].style.opacity = "1";
             } else {
-                imgs[0].style.opacity = '1';
+                imgs[0].style.opacity = "1";
             }
             break;
         }
@@ -17,16 +20,16 @@ function nextImage(imgs) {
     setTimeout(nextImage, 5000, imgs);
 }
 
-var figures = document.getElementsByClassName('slideshow');
+var figures = document.getElementsByClassName("slideshow");
 function figuresSlide(i) {
     return function () {
-        nextImage(figures[i].getElementsByTagName('img'));
+        nextImage(figures[i].getElementsByTagName("img"));
     };
 }
 
 if (figures != null) {
-    for (var i = 0; i < figures.length; i++) {
-        if (figures[i].className == 'slideshow') {
+    for (let i = 0; i < figures.length; i++) {
+        if (figures[i].className == "slideshow") {
             var wrapper = figuresSlide(i);
         }
         setTimeout(wrapper(i), 5000);
