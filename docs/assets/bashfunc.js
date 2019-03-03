ec014c899ff4281900502f24541f35f685d8ca74
Brandon Dunneman <bwfang1235@gmail.com>
2019-03-02 22:59:31 -0800

    Sticky support stripped out
    * So originally the plan was to manipulate the DOM to get the extra tags needed in there. You know where that went? Absolutely nowhere
    * Turns out it's not easy to add nodes to a page when you need to work with parent and child nodes, and those don't... exist. Because the page was coded flat. No containers.
    * SO! Stickies stripped out, but the next plan is on the way:
    Fixed nav menu yo!

--- docs/assets/bashfunc.js
+++ docs/assets/bashfunc.js
@@ -2,25 +2,24 @@
 /*jslint browser:true*/
 "use strict";
 
-var supportCSS = Boolean((window.CSS && window.CSS.supports) || window.supportsCSS || false);
-var supportCSSRule = Boolean((window.CSSRule) || false);
-var supportAtSup = Boolean((window.CSSRule.SUPPORTS_RULE) || false);
-
+// Support vars
+var wbSupportCSS = Boolean((window.CSS && window.CSS.supports) || window.supportsCSS || false);
+var wbSupportCSSRule = Boolean((window.CSSRule) || false);
+var wbSupportAtSup = Boolean((window.CSSRule.SUPPORTS_RULE) || false);
+// Support functions
 function hasAtSup() {
-//  console.log("CSS Support:", supportCSS, ", CSSRule Support:", supportCSSRule, ", Has @support:", supportAtSup);
-  return supportCSS && supportCSSRule && supportAtSup;
+//  console.log("CSS Support:", wbSupportCSS, ", CSSRule Support:", wbSupportCSSRule, ", Has @support:", wbSupportAtSup);
+  return wbSupportCSS && wbSupportCSSRule && wbSupportAtSup;
 }
-
-function hasRuleSup(rule1, rule2) {
-//  console.log("Checking support for:  \"",rule1,":",rule2,"\" =",CSS.supports(rule1, rule2));
-  return CSS.supports(rule1, rule2);
+function hasRuleSup(wbRule1, wbRule2) {
+//  console.log("Checking support for:  \"",wbRule1,":",wbRule2,"\" =",CSS.supports(wbRule1, wbRule2));
+  return CSS.supports(wbRule1, wbRule2);
 }
 
 
 /*
 Slideshow Code
 */
-
 function nextImage(imgs) {
     for (var i = 0; i < imgs.length; i++) {
         if (imgs[0].style.opacity == "") {
@@ -39,37 +38,48 @@ function nextImage(imgs) {
     }
     setTimeout(nextImage, 5000, imgs);
 }
-
-var figures = document.getElementsByClassName("slideshow");
+var wbFigures = document.getElementsByClassName("slideshow");
 function figuresSlide(i) {
     return function () {
-        nextImage(figures[i].getElementsByTagName("img"));
+        nextImage(wbFigures[i].getElementsByTagName("img"));
     };
 }
-
-if (figures != null) {
-    for (var i = 0; i < figures.length; i++) {
-        if (figures[i].className == "slideshow") {
-            var wrapper = figuresSlide(i);
+if (wbFigures != null) {
+    for (var i = 0; i < wbFigures.length; i++) {
+        if (wbFigures[i].className == "slideshow") {
+            var wbWrapper = figuresSlide(i);
         }
-        setTimeout(wrapper(i), 5000);
+        setTimeout(wbWrapper(i), 5000);
     }
 }
 
-
-/*
+/* 
 Sticky Code
-*/
 
 if (hasRuleSup("position","sticky")) {
-    var h2t = document.getElementsByTagName("h2");
-    var h3t = document.getElementsByTagName("h3");
-    var h3h = h2t[0].offsetHeight;
-
-    Object.keys(h3t).forEach(function(index) {
-        h3t[index].style.top = h3h - 2 + "px";
-    });    
-
+// Gathering vars for sticky positioning
+    var wbH1T = document.getElementsByTagName("H1");
+    var wbH2T = document.getElementsByTagName("H2");
+    var wbH3T = document.getElementsByTagName("H3");
+// Resource vars for sticky positioning
+    var wbH3H = wbH2T[0].offsetHeight;
+// Setting H2 styles
+    Object.keys(wbH2T).forEach(function(index) {
+        wbH2T[index].style.marginBottom = - 1 + "px";
+        wbH2T[index].style.marginTop = "0";
+        wbH2T[index].style.position = "sticky";
+        wbH2T[index].style.top =  - 1 + "px";
+        wbH2T[index].style.zIndex = 100;
+    });
+// Setting H3 styles
+    Object.keys(wbH3T).forEach(function(index) {
+        wbH3T[index].style.marginBottom = - 1 + "px";
+        wbH3T[index].style.marginTop = "0";
+        wbH3T[index].style.position = "sticky";
+        wbH3T[index].style.top = wbH3H - 2 + "px";
+        wbH3T[index].style.zIndex = 50;
+    });
+// Setting anchor link offsets for stickied H3s
     document.addEventListener('click', function(event) {
         if (event.target.tagName !== 'A') return;
         var href = event.target.getAttribute('href') || '';
@@ -78,15 +88,62 @@ if (hasRuleSup("position","sticky")) {
         if (!target || target.tagName !== 'H3') return;
         setTimeout(offsetAnchor, 10);
     });
+// Offsets readme cross-page anchor links
+    window.setTimeout(offsetAnchor, 5);
+// String vars for sticky blocks and spacing
+    var wbBodyStart = "<wbchap><wbsect>";
+    var wbH2Before = "<wbcontend></wbcontend></wbsect></wbchap><wbchap>"
+    var wbH2After = "<wbcontbgn></wbcontbgn><wbsect>"
+    var wbH3Before = "<wbcontend></wbcontend></wbsect><wbsect>"
+    var wbH3After = "<wbcontbgn></wbcontbgn>"
+    var wbBodyEnd = "<wbcontend></wbcontend></wbsect></wbchap>";
+// Applying boxes and spacers
+    Object.keys(wbH1T).forEach(function(index) {
+        wbH1T[index].insertAdjacentHTML('afterend', wbBodyStart);
+    });
+    Object.keys(wbH2T).forEach(function(index) {
+        wbH2T[index].insertAdjacentHTML('beforebegin', wbH2Before);
+        wbH2T[index].insertAdjacentHTML('afterend', wbH2After);
+    });
+    Object.keys(wbH3T).forEach(function(index) {
+        wbH3T[index].insertAdjacentHTML('beforebegin', wbH3Before);
+        wbH3T[index].insertAdjacentHTML('afterend', wbH3After);
+    });
+    document.body.insertAdjacentHTML('beforeend', wbBodyEnd);
+// Block vars
+    var wbChp = document.getElementsByTagName("wbchap");
+    var wbSct = document.getElementsByTagName("wbsect");
+// Spacer vars
+    var wbCB = document.getElementsByTagName("wbcontbgn");
+    var wbCE = document.getElementsByTagName("wbcontend");
+    // Applying box and spacer styles
+    Object.keys(wbChp).forEach(function(index) {
+        wbChp[index].style.display = "block";
+    });
+    Object.keys(wbSct).forEach(function(index) {
+        wbSct[index].style.display = "block";
+    });
+    Object.keys(wbCB).forEach(function(index) {
+        wbCB[index].style.clear = "both";
+        wbCB[index].style.content = "";
+        wbCB[index].style.display = "block";
+        wbCB[index].style.marginBottom = 1 + "rem";
+    });
+    Object.keys(wbCE).forEach(function(index) {
+        wbCE[index].style.clear = "both";
+        wbCE[index].style.content = "";
+        wbCE[index].style.display = "block";
+        wbCE[index].style.marginBottom = 1.6 + "rem";
+    });
 
-    window.setTimeout(offsetAnchor, 0);
 }
 else {
     throw new Error("\"position: sticky\" isn't supported by this browser.");
 }
-
+// Archor link offset function
 function offsetAnchor() {
     if (location.hash.length !== 0) {
-        window.scrollTo(window.scrollX, window.scrollY - (h3h - 2));
+        window.scrollTo(window.scrollX, window.scrollY - (wbH3H - 2));
   }
-}
+} 
+*/
